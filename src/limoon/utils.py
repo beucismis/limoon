54e8d2c65256bc3df958a6d45e2c2e0063f1c69a
beucismis <beucismis@tutamail.com>
2025-09-14 20:37:58 +0300

    Update utils.py

--- src/limoon/utils.py
+++ src/limoon/utils.py
@@ -1,11 +1,12 @@
-from typing import Iterator
+from urllib.parse import urlparse
+from typing import Optional, Iterator
 
 from requests_html import HTML
 
 from . import model
 
 
-def entry_parser(html: HTML, max_entry: int = None) -> Iterator[model.Entry]:
+def entry_parser(html: HTML, max_entry: Optional[int] = None) -> Iterator[model.Entry]:
     entry_items = html.find("ul#entry-item-list", first=True).find("li#entry-item")
 
     for item in entry_items[:max_entry]:
@@ -13,6 +14,7 @@ def entry_parser(html: HTML, max_entry: int = None) -> Iterator[model.Entry]:
         content = item.find("div.content", first=True)
         favorite_count = item.attrs["data-favorite-count"]
         created = item.find("a.entry-date", first=True)
+        topic_title = html.find("h1#title", first=True)
 
         yield model.Entry(
             int(item.attrs["data-id"]),
@@ -20,4 +22,6 @@ def entry_parser(html: HTML, max_entry: int = None) -> Iterator[model.Entry]:
             content.text,
             int(favorite_count),
             created.text,
+            topic_title.attrs["data-title"],
+            urlparse(topic_title.find("a", first=True).attrs["href"]).path.split("/")[-1]
         )
