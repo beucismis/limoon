764e89db50619b175c8471e2a85e0a062986e087
beucismis <beucismis@tutamail.com>
2025-09-14 23:43:20 +0300

    Update core.py

--- src/limoon/core.py
+++ src/limoon/core.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
-from typing import Callable, Iterator, TypeVar, Optional, Union
+from typing import Callable, Iterator, Optional, TypeVar, Union
 from urllib.parse import urlparse
 
 import requests
 from requests_html import HTMLResponse, HTMLSession
 
-from . import constant, exception, model, utils
+from . import constants, exceptions, models, utils
 
 
 # Typings
@@ -20,15 +20,15 @@ session = HTMLSession()
 
 def request(endpoint: str, headers: dict = {}, params: dict = {}) -> requests.Response:
     return session.get(
-        constant.BASE_URL + endpoint,
+        constants.BASE_URL + endpoint,
         params=params,
-        headers=constant.HEADERS,
+        headers=constants.HEADERS,
     )
 
 
 def get_topic(
     topic_keywords: TopicKeywords, page: int = 1, max_entry: Optional[int] = None
-) -> model.Topic:
+) -> models.Topic:
     """This function get Ekşi Sözlük topic.
 
     Arguments:
@@ -37,22 +37,22 @@ def get_topic(
     max_entry (int|None): Max entry per topic.
 
     Returns:
-    model.Topic (class): Topic data class.
+    models.Topic (class): Topic data class.
     """
 
     if not isinstance(page, int):
         raise TypeError
 
-    r = request(constant.TOPIC_ROUTE.format(topic_keywords), params={"p": page})
+    r = request(constants.TOPIC_ROUTE.format(topic_keywords), params={"p": page})
 
     if r.status_code == HTTPStatus.NOT_FOUND:
-        raise exception.TopicNotFound()
+        raise exceptions.TopicNotFound()
 
     h1 = r.html.find("h1#title", first=True)
     path = h1.find("a", first=True).attrs["href"]
     page_count = r.html.find("div.pager", first=True)
 
-    return model.Topic(
+    return models.Topic(
         int(h1.attrs["data-id"]),
         h1.attrs["data-title"],
         path[1:],
@@ -61,43 +61,43 @@ def get_topic(
     )
 
 
-def get_entry(entry_id: EntryID) -> model.Entry:
+def get_entry(entry_id: EntryID) -> models.Entry:
     """This function get Ekşi Sözlük entry.
 
     Arguments:
     entry_id (int): Unique entry identity.
 
     Returns:
-    model.Entry (class): Entry data class.
+    models.Entry (class): Entry data class.
     """
 
     if not isinstance(entry_id, int):
         raise TypeError
 
-    r = request(constant.ENTRY_ROUTE.format(entry_id))
+    r = request(constants.ENTRY_ROUTE.format(entry_id))
 
     if r.status_code == HTTPStatus.NOT_FOUND:
-        raise exception.EntryNotFound()
-    if r.html.find("h1", first=True).text == exception.SHIT_MESSAGE:
-        raise exception.EntryNotFound()
+        raise exceptions.EntryNotFound()
+    if r.html.find("h1", first=True).text == exceptions.SHIT_MESSAGE:
+        raise exceptions.EntryNotFound()
 
     return next(utils.entry_parser(r.html))
 
 
-def get_author(nickname: Nickname) -> model.Author:
+def get_author(nickname: Nickname) -> models.Author:
     """This function get Ekşi Sözlük author.
 
     Arguments:
     nickname (str): Unique author nickname.
 
     Returns:
-    model.Author (class): Author data class.
+    models.Author (class): Author data class.
     """
 
-    r = request(constant.AUTHOR_ROUTE.format(nickname))
+    r = request(constants.AUTHOR_ROUTE.format(nickname))
 
     if r.status_code == HTTPStatus.NOT_FOUND:
-        raise exception.AuthorNotFound()
+        raise exceptions.AuthorNotFound()
 
     nickname = r.html.find("h1#user-profile-title", first=True)
     biography = r.html.find("div#profile-biography", first=True)
@@ -108,7 +108,7 @@ def get_author(nickname: Nickname) -> model.Author:
     avatar_url = r.html.find("img.avatar", first=True)
     rank = r.html.find("p.muted", first=True)
 
-    return model.Author(
+    return models.Author(
         nickname.attrs["data-nick"],
         biography.text if biography else biography,
         int(total_entry.text),
@@ -120,62 +120,62 @@ def get_author(nickname: Nickname) -> model.Author:
     )
 
 
-def get_author_rank(nickname: Nickname) -> model.Rank:
+def get_author_rank(nickname: Nickname) -> models.Rank:
     """This function get Ekşi Sözlük author rank.
 
     Arguments:
     nickname (str): Unique author nickname.
 
     Returns:
-    model.Rank (class): Rank data class.
+    models.Rank (class): Rank data class.
     """
 
     author = get_author(nickname)
 
-    return model.Rank(name=author.rank.name, karma=author.rank.karma)
+    return models.Rank(name=author.rank.name, karma=author.rank.karma)
 
 
-def get_author_badges(nickname: Nickname) -> Iterator[model.Badge]:
+def get_author_badges(nickname: Nickname) -> Iterator[models.Badge]:
     """This function get Ekşi Sözlük author badges.
 
     Arguments:
     nickname (str): Unique author nickname.
 
     Returns:
-    Iterator[model.Badge]: Badge data classes.
+    Iterator[models.Badge]: Badge data classes.
     """
 
-    r = request(constant.AUTHOT_BADGES_TOPIC.format(nickname))
+    r = request(constants.AUTHOT_BADGES_ROUTE.format(nickname))
 
     for badge in r.html.find("li.badge-item-otheruser"):
         if badge.attrs["data-owned"] != "False":
-            yield model.Badge(
+            yield models.Badge(
                 badge.find("p", first=True).text,  # name
-                badge.find("a", first=True).attrs["data-title"],  # description
-                badge.find("img", first=True).attrs["src"],  # icon_url
+                badge.find("a", first=True).attrs["data-title"],
+                badge.find("img", first=True).attrs["src"],
             )
 
 
-def get_author_topic(nickname: Nickname) -> model.Topic:
+def get_author_topic(nickname: Nickname) -> models.Topic:
     """This function get Ekşi Sözlük author topic.
 
     Arguments:
     nickname (str): Unique author nickname.
 
     Returns:
-    model.Topic (class): Topic data class.
+    models.Topic (class): Topic data class.
     """
 
-    r = request(constant.AUTHOR_TOPIC_ROUTE.format(nickname))
+    r = request(constants.AUTHOR_TOPIC_ROUTE.format(nickname))
 
     return get_topic(urlparse(r.url).path[1:])
 
 
-def get_author_last_entrys(nickname: Nickname, page: int = 1) -> Iterator[model.Entry]:
+def get_author_last_entrys(nickname: Nickname, page: int = 1) -> Iterator[models.Entry]:
     """ """
 
     r = request(
-        constant.AUTHOR_LAST_ENTRYS,
+        constants.AUTHOR_LAST_ENTRYS_ROUTE,
         params={"nick": nickname, "p": page},
     )
 
@@ -185,64 +185,64 @@ def get_author_last_entrys(nickname: Nickname, page: int = 1) -> Iterator[model.
         yield get_entry(int(topic.find("li#entry-item", first=True).attrs["data-id"]))
 
 
-def get_agenda(max_topic: Optional[int] = None) -> Iterator[model.Agenda]:
+def get_agenda(max_topic: Optional[int] = None) -> Iterator[models.Agenda]:
     """This function get Ekşi Sözlük agenda (gündem) page.
 
     Arguments:
     max_topic (int=None): Maximum number of topics get from agenda.
 
     Returns:
-    Iterator[model.Agenda]: Agenda data classes.
+    Iterator[models.Agenda]: Agenda data classes.
     """
 
-    r = request(constant.AGENDA_ROUTE)
+    r = request(constants.AGENDA_ROUTE)
 
     if r.status_code != HTTPStatus.OK:
-        raise exception.TopicNotFound()
+        raise exceptions.TopicNotFound()
 
     topic_list = r.html.find("ul.topic-list", first=True).find("a")
 
     for topic in topic_list[:max_topic]:
-        yield model.Agenda(
+        yield models.Agenda(
             topic.text.rsplit(None, 1)[0] if " " in topic.text else "",
             urlparse(topic.attrs["href"]).path.split("/")[-1],
             topic.find("small", first=True).text,
         )
 
 
-def get_debe() -> Iterator[model.Debe]:
+def get_debe() -> Iterator[models.Debe]:
     """This function get Ekşi Sözlük debe page.
 
     Returns:
-    Iterator[model.Debe]: Entry data classes.
+    Iterator[models.Debe]: Entry data classes.
     """
 
-    r = request(constant.DEBE_ROUTE)
+    r = request(constants.DEBE_ROUTE)
 
     if r.status_code != HTTPStatus.OK:
-        raise exception.EntryNotFound()
+        raise exceptions.EntryNotFound()
 
     entry_list = r.html.find("ul.topic-list", first=True).find("a")
 
     for entry in entry_list:
-        yield model.Debe(
+        yield models.Debe(
             entry.find("span.caption", first=True).text,
             urlparse(entry.attrs["href"]).path.split("/")[-1],
         )
 
 
-def get_search_topic(keywords: SearchKeywords) -> Iterator[model.SearchResult]:
+def get_search_topic(keywords: SearchKeywords) -> Iterator[models.SearchResult]:
     """This function get Ekşi Sözlük search topic page.
 
     Arguments:
     keywords (SearchKeywords): Search keywords.
 
     Returns:
-    Iterator[model.SearchResult]: SearchResult data classes.
+    Iterator[models.SearchResult]: SearchResult data classes.
     """
 
     r = request(
-        constant.SEARCH_ROUTE,
+        constants.SEARCH_ROUTE,
         params={
             "SearchForm.Keywords": keywords,
             "SearchForm.NiceOnly": "false",
@@ -250,10 +250,15 @@ def get_search_topic(keywords: SearchKeywords) -> Iterator[model.SearchResult]:
         },
     )
 
-    topic_list = r.html.find("ul.topic-list", first=True).find("a")
+    topic_ul = r.html.find("ul.topic-list", first=True)
+
+    if not topic_ul:
+        raise exceptions.SearchResultNotFound()
+
+    topic_list = topic_ul.find("a")
 
     for topic in topic_list:
-        yield model.SearchResult(
+        yield models.SearchResult(
             topic.text.rsplit(None, 1)[0] if " " in topic.text else "",
             urlparse(topic.attrs["href"]).path.split("/")[-1],
             topic.find("small", first=True).text,
